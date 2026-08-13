1d368b33049ea0c8e4216feb2628fe7a64152d30
vcl <251510552+vuong4-design@users.noreply.github.com>
2026-08-13 20:10:03 +0700

    Update NhiepThiTranRouteHelper.cs

--- auto/ns26/NhiepThiTranRouteHelper.cs
+++ auto/ns26/NhiepThiTranRouteHelper.cs
@@ -5,311 +5,311 @@ namespace ns26;
 
 internal class NhiepThiTranRouteHelper
 {
-	private static uint[,] uint_0;
+	private static uint[,] map80RouteVariant1Coordinates;
 
-	private static uint[,] uint_1;
+	private static uint[,] map80RouteVariant2Coordinates;
 
-	private static uint[,] uint_2;
+	private static uint[,] map80RouteVariant3Coordinates;
 
-	private static uint[,] uint_3;
+	private static uint[,] map80RouteVariant4Coordinates;
 
-	private static uint[,] uint_4;
+	private static uint[,] map80RouteVariant5Coordinates;
 
-	private static GStruct23[] gstruct23_0;
+	private static GStruct23[] map80RouteCandidates;
 
-	private static uint[,] uint_5;
+	private static uint[,] map11RouteVariant1Coordinates;
 
-	private static uint[,] uint_6;
+	private static uint[,] map11RouteVariant2Coordinates;
 
-	private static uint[,] uint_7;
+	private static uint[,] map11RouteVariant3Coordinates;
 
-	private static uint[,] uint_8;
+	private static uint[,] map11RouteVariant4Coordinates;
 
-	private static GStruct23[] gstruct23_1;
+	private static GStruct23[] map11RouteCandidates;
 
-	private static uint[,] uint_9;
+	private static uint[,] map1RouteVariant1Coordinates;
 
-	private static uint[,] uint_10;
+	private static uint[,] map1RouteVariant2Coordinates;
 
-	private static uint[,] uint_11;
+	private static uint[,] map1RouteVariant3Coordinates;
 
-	private static uint[,] uint_12;
+	private static uint[,] map1RouteVariant4Coordinates;
 
-	private static GStruct23[] gstruct23_2;
+	private static GStruct23[] map1RouteCandidates;
 
-	private static uint[,] uint_13;
+	private static uint[,] map37RouteVariant1Coordinates;
 
-	private static uint[,] uint_14;
+	private static uint[,] map37RouteVariant2Coordinates;
 
-	private static uint[,] uint_15;
+	private static uint[,] map37RouteVariant3Coordinates;
 
-	private static uint[,] uint_16;
+	private static uint[,] map37RouteVariant4Coordinates;
 
-	private static GStruct23[] gstruct23_3;
+	private static GStruct23[] map37RouteCandidates;
 
-	private static uint[,] uint_17;
+	private static uint[,] map162RouteVariant1Coordinates;
 
-	private static uint[,] uint_18;
+	private static uint[,] map162RouteVariant2Coordinates;
 
-	private static uint[,] uint_19;
+	private static uint[,] map162RouteVariant3Coordinates;
 
-	private static uint[,] uint_20;
+	private static uint[,] map162RouteVariant4Coordinates;
 
-	private static GStruct23[] gstruct23_4;
+	private static GStruct23[] map162RouteCandidates;
 
-	private static uint[,] uint_21;
+	private static uint[,] map78RouteVariant1Coordinates;
 
-	private static uint[,] uint_22;
+	private static uint[,] map78RouteVariant2Coordinates;
 
-	private static uint[,] uint_23;
+	private static uint[,] map78RouteVariant3Coordinates;
 
-	private static uint[,] uint_24;
+	private static uint[,] map78RouteVariant4Coordinates;
 
-	private static uint[,] uint_25;
+	private static uint[,] map78RouteVariant5Coordinates;
 
-	private static uint[,] uint_26;
+	private static uint[,] map78RouteVariant6Coordinates;
 
-	private static GStruct23[] gstruct23_5;
+	private static GStruct23[] map78RouteCandidates;
 
-	private static uint[,] uint_27;
+	private static uint[,] map176RouteVariant1Coordinates;
 
-	private static uint[,] uint_28;
+	private static uint[,] map176RouteVariant2Coordinates;
 
-	private static uint[,] uint_29;
+	private static uint[,] map176RouteVariant3Coordinates;
 
-	private static GStruct23[] gstruct23_6;
+	private static GStruct23[] map176RouteCandidates;
 
 	static NhiepThiTranRouteHelper()
 	{
 		uint[,] array_ = new uint[20, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_, 54488);
-		uint_0 = array_;
+		map80RouteVariant1Coordinates = array_;
 		uint[,] array_2 = new uint[13, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_2, 54658);
-		uint_1 = array_2;
+		map80RouteVariant2Coordinates = array_2;
 		uint[,] array_3 = new uint[14, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_3, 54771);
-		uint_2 = array_3;
+		map80RouteVariant3Coordinates = array_3;
 		uint[,] array_4 = new uint[20, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_4, 54892);
-		uint_3 = array_4;
+		map80RouteVariant4Coordinates = array_4;
 		uint[,] array_5 = new uint[7, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_5, 55062);
-		uint_4 = array_5;
-		gstruct23_0 = new GStruct23[5]
+		map80RouteVariant5Coordinates = array_5;
+		map80RouteCandidates = new GStruct23[5]
 		{
 			new GStruct23
 			{
-				uint_0 = uint_0
+				uint_0 = map80RouteVariant1Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_1
+				uint_0 = map80RouteVariant2Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_2
+				uint_0 = map80RouteVariant3Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_3
+				uint_0 = map80RouteVariant4Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_4
+				uint_0 = map80RouteVariant5Coordinates
 			}
 		};
 		uint[,] array_6 = new uint[21, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_6, 55151);
-		uint_5 = array_6;
+		map11RouteVariant1Coordinates = array_6;
 		uint[,] array_7 = new uint[20, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_7, 55329);
-		uint_6 = array_7;
+		map11RouteVariant2Coordinates = array_7;
 		uint[,] array_8 = new uint[25, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_8, 55499);
-		uint_7 = array_8;
+		map11RouteVariant3Coordinates = array_8;
 		uint[,] array_9 = new uint[16, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_9, 55709);
-		uint_8 = array_9;
-		gstruct23_1 = new GStruct23[4]
+		map11RouteVariant4Coordinates = array_9;
+		map11RouteCandidates = new GStruct23[4]
 		{
 			new GStruct23
 			{
-				uint_0 = uint_5
+				uint_0 = map11RouteVariant1Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_6
+				uint_0 = map11RouteVariant2Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_7
+				uint_0 = map11RouteVariant3Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_8
+				uint_0 = map11RouteVariant4Coordinates
 			}
 		};
 		uint[,] array_10 = new uint[6, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_10, 55867);
-		uint_9 = array_10;
+		map1RouteVariant1Coordinates = array_10;
 		uint[,] array_11 = new uint[8, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_11, 55924);
-		uint_10 = array_11;
+		map1RouteVariant2Coordinates = array_11;
 		uint[,] array_12 = new uint[14, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_12, 55997);
-		uint_11 = array_12;
+		map1RouteVariant3Coordinates = array_12;
 		uint[,] array_13 = new uint[15, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_13, 56118);
-		uint_12 = array_13;
-		gstruct23_2 = new GStruct23[4]
+		map1RouteVariant4Coordinates = array_13;
+		map1RouteCandidates = new GStruct23[4]
 		{
 			new GStruct23
 			{
-				uint_0 = uint_9
+				uint_0 = map1RouteVariant1Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_10
+				uint_0 = map1RouteVariant2Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_11
+				uint_0 = map1RouteVariant3Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_12
+				uint_0 = map1RouteVariant4Coordinates
 			}
 		};
 		uint[,] array_14 = new uint[22, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_14, 56267);
-		uint_13 = array_14;
+		map37RouteVariant1Coordinates = array_14;
 		uint[,] array_15 = new uint[31, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_15, 56453);
-		uint_14 = array_15;
+		map37RouteVariant2Coordinates = array_15;
 		uint[,] array_16 = new uint[25, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_16, 56711);
-		uint_15 = array_16;
+		map37RouteVariant3Coordinates = array_16;
 		uint[,] array_17 = new uint[13, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_17, 56921);
-		uint_16 = array_17;
-		gstruct23_3 = new GStruct23[4]
+		map37RouteVariant4Coordinates = array_17;
+		map37RouteCandidates = new GStruct23[4]
 		{
 			new GStruct23
 			{
-				uint_0 = uint_13
+				uint_0 = map37RouteVariant1Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_14
+				uint_0 = map37RouteVariant2Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_15
+				uint_0 = map37RouteVariant3Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_16
+				uint_0 = map37RouteVariant4Coordinates
 			}
 		};
 		uint[,] array_18 = new uint[16, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_18, 57054);
-		uint_17 = array_18;
+		map162RouteVariant1Coordinates = array_18;
 		uint[,] array_19 = new uint[13, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_19, 57192);
-		uint_18 = array_19;
+		map162RouteVariant2Coordinates = array_19;
 		uint[,] array_20 = new uint[7, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_20, 57305);
-		uint_19 = array_20;
+		map162RouteVariant3Coordinates = array_20;
 		uint[,] array_21 = new uint[10, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_21, 57370);
-		uint_20 = array_21;
-		gstruct23_4 = new GStruct23[4]
+		map162RouteVariant4Coordinates = array_21;
+		map162RouteCandidates = new GStruct23[4]
 		{
 			new GStruct23
 			{
-				uint_0 = uint_17
+				uint_0 = map162RouteVariant1Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_18
+				uint_0 = map162RouteVariant2Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_19
+				uint_0 = map162RouteVariant3Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_20
+				uint_0 = map162RouteVariant4Coordinates
 			}
 		};
 		uint[,] array_22 = new uint[16, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_22, 57479);
-		uint_21 = array_22;
+		map78RouteVariant1Coordinates = array_22;
 		uint[,] array_23 = new uint[6, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_23, 57617);
-		uint_22 = array_23;
+		map78RouteVariant2Coordinates = array_23;
 		uint[,] array_24 = new uint[11, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_24, 57674);
-		uint_23 = array_24;
+		map78RouteVariant3Coordinates = array_24;
 		uint[,] array_25 = new uint[6, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_25, 57771);
-		uint_24 = array_25;
+		map78RouteVariant4Coordinates = array_25;
 		uint[,] array_26 = new uint[11, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_26, 57828);
-		uint_25 = array_26;
+		map78RouteVariant5Coordinates = array_26;
 		uint[,] array_27 = new uint[16, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_27, 57925);
-		uint_26 = array_27;
-		gstruct23_5 = new GStruct23[6]
+		map78RouteVariant6Coordinates = array_27;
+		map78RouteCandidates = new GStruct23[6]
 		{
 			new GStruct23
 			{
-				uint_0 = uint_21
+				uint_0 = map78RouteVariant1Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_22
+				uint_0 = map78RouteVariant2Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_23
+				uint_0 = map78RouteVariant3Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_24
+				uint_0 = map78RouteVariant4Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_25
+				uint_0 = map78RouteVariant5Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_26
+				uint_0 = map78RouteVariant6Coordinates
 			}
 		};
 		uint[,] array_28 = new uint[54, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_28, 58091);
-		uint_27 = array_28;
+		map176RouteVariant1Coordinates = array_28;
 		uint[,] array_29 = new uint[37, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_29, 58533);
-		uint_28 = array_29;
+		map176RouteVariant2Coordinates = array_29;
 		uint[,] array_30 = new uint[27, 2];
 		EmbeddedResourceDataReader.CopyLengthPrefixedBlock(array_30, 58839);
-		uint_29 = array_30;
-		gstruct23_6 = new GStruct23[3]
+		map176RouteVariant3Coordinates = array_30;
+		map176RouteCandidates = new GStruct23[3]
 		{
 			new GStruct23
 			{
-				uint_0 = uint_27
+				uint_0 = map176RouteVariant1Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_28
+				uint_0 = map176RouteVariant2Coordinates
 			},
 			new GStruct23
 			{
-				uint_0 = uint_29
+				uint_0 = map176RouteVariant3Coordinates
 			}
 		};
 	}
@@ -324,25 +324,25 @@ internal class NhiepThiTranRouteHelper
 		switch (int_0)
 		{
 		case 37:
-			result = MapTravelDataHelper.SelectBestRouteCoordinates(gstruct23_3, uint_30, uint_31);
+			result = MapTravelDataHelper.SelectBestRouteCoordinates(map37RouteCandidates, uint_30, uint_31);
 			break;
 		case 11:
-			result = MapTravelDataHelper.SelectBestRouteCoordinates(gstruct23_1, uint_30, uint_31);
+			result = MapTravelDataHelper.SelectBestRouteCoordinates(map11RouteCandidates, uint_30, uint_31);
 			break;
 		case 1:
-			result = MapTravelDataHelper.SelectBestRouteCoordinates(gstruct23_2, uint_30, uint_31);
+			result = MapTravelDataHelper.SelectBestRouteCoordinates(map1RouteCandidates, uint_30, uint_31);
 			break;
 		case 80:
-			result = MapTravelDataHelper.SelectBestRouteCoordinates(gstruct23_0, uint_30, uint_31);
+			result = MapTravelDataHelper.SelectBestRouteCoordinates(map80RouteCandidates, uint_30, uint_31);
 			break;
 		case 78:
-			result = MapTravelDataHelper.SelectBestRouteCoordinates(gstruct23_5, uint_30, uint_31);
+			result = MapTravelDataHelper.SelectBestRouteCoordinates(map78RouteCandidates, uint_30, uint_31);
 			break;
 		case 176:
-			result = MapTravelDataHelper.SelectBestRouteCoordinates(gstruct23_6, uint_30, uint_31);
+			result = MapTravelDataHelper.SelectBestRouteCoordinates(map176RouteCandidates, uint_30, uint_31);
 			break;
 		case 162:
-			result = MapTravelDataHelper.SelectBestRouteCoordinates(gstruct23_4, uint_30, uint_31);
+			result = MapTravelDataHelper.SelectBestRouteCoordinates(map162RouteCandidates, uint_30, uint_31);
 			break;
 		}
 		return result;
